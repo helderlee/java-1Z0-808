78c9d72295525f3da2cca3b63ced017c68006da3
helderlee <helderlee@yahoo.com>
2018-11-05 15:23:09 -0200

    Comments and javadoc.

---
+++ L010-Comments/src/main/java/com/helderlee/Comment.java
@@ -0,0 +1,25 @@
+package com.helderlee;
+
+
+public class Comment {
+
+    // Single comment
+
+    /*
+    * Multiline comment
+    **/
+    private String comment;
+
+
+    /**
+     * Method description.
+     *
+     * @param paramIn param description.
+     *
+     * @return return description.
+     */
+    public String hello(String paramIn) {
+        System.out.printf("");
+        return null;
+    }
+}
