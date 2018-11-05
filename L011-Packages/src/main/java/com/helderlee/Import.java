90c73bed3237c02a7ff7187f8583aa62ebba630c
helderlee <helderlee@yahoo.com>
2018-11-05 16:38:03 -0200

    Packages

---
+++ L011-Packages/src/main/java/com/helderlee/Import.java
@@ -0,0 +1,13 @@
+package com.helderlee;
+
+import java.util.Random;
+
+import static java.lang.System.out;
+
+public class Import {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        out.println(random.nextInt(5));
+    }
+}
