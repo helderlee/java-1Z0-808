b1cd61d8bdb1e9efb09c248f3ec2b34501e67535
Helder Lee <helderlee@yahoo.com>
2018-11-11 16:57:07 -0200

    Primitive char and boolean

---
+++ L017-PrimitiveCharBoolean/src/main/java/com/helderlee/PrimitiveCharBoolean.java
@@ -0,0 +1,16 @@
+package com.helderlee;
+
+public class PrimitiveCharBoolean {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        char c1 = '1';
+        char uc = '\u03A9';
+        System.out.println(uc);
+        char rc = '\u216C';
+        System.out.println(rc);
+
+        boolean myBool = true;
+        boolean myFalse = false;
+    }
+}
